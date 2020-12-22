40b6b56a0635774aad1c63126f7bd13a68185ab1
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2020-12-22 22:09:40 +0100

    start with fly controller

---
+++ Assets/Scripts/FlyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class FlyController : MonoBehaviour
+{
+    float speed;
+    float moveAmount; 
+    public Transform trans;
+    public Rigidbody rb;
+   // public SteamVR_Action_Boolean trigger;
+    public SteamVR_Action_Vector2 joystickInput;
+ 
+    public KeyCode schub;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = gameObject.transform; 
+        rb = GetComponent<Rigidbody>();
+    }
+
+   
+    void movePlayer()
+    {
+        moveAmount = speed * joystickInput.GetAxis(SteamVR_Input_Sources.LeftHand).y;
+        
+        rb.AddForce(moveForce);
+    }
+}
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 joystick_
+        {
+            get
+            {
+                return SteamVR_Actions.default_joystick_;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
