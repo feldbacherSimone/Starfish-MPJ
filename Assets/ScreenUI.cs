4710d7c1130e7e5fe5370c91ddfb26b53033c78d
feldbacherSimone <simone.feldbacher03@htl-salzburg-ac.at>
2021-04-14 17:57:16 +0200

    Start working on ship screens

---
+++ Assets/ScreenUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScreenUI : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshPro repairPercentTEXT;
+    [SerializeField]
+    int destuctionPercent;
+    
+
+    private void Update()
+    {
+        repairPercentTEXT.text = destuctionPercent.ToString() + "%";
+    }
+}
