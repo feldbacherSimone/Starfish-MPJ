a0670bf73f126a3194143de2248529b093893b7a
feldbacherSimone <simone.feldbacher03@htl-salzburg-ac.at>
2021-04-21 18:00:32 +0200

    start Inventory Logic

---
+++ Assets/CraftingDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingDisplay : MonoBehaviour
+{
+    [SerializeField]
+    public Ingredient[] ingredients;
+
+    public GameObject listTemplate;
+
+      private  void CreateRecipy()
+    {
+        foreach (Ingredient ingredient in ingredients)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Ingredients.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    int id;
+    string name;
+    Sprite sprite; 
+
+    public Item(int id, string name, Sprite sprite)
+    {
+        this.id = id;
+        this.name = name;
+        this.sprite = sprite;
+    }
+
+}
+[System.Serializable]
+public class Ingredient
+{
+   public  Item item;
+   public int ammount;
+}
