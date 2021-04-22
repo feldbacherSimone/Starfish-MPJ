05771735588f3a7f2bc079b654387a660fe3f889
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-04-22 20:48:23 +0200

    Implement Crafting Display

---
+++ Assets/Scripts/CraftingDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class CraftingDisplay : MonoBehaviour
+{
+    [SerializeField]
+    public Ingredient[] ingredients;
+
+    public GameObject listTemplate;
+    public Transform positionRefernce;
+
+    [SerializeField]
+    float offsetammount;
+
+    private void Start()
+    {
+        CreateRecipy(); ;
+    }
+
+    private  void CreateRecipy()
+    {
+
+        float offset = 0; 
+        foreach (Ingredient ingredient in ingredients)
+        {
+            GameObject newLine = GameObject.Instantiate(listTemplate, positionRefernce.transform);
+            newLine.transform.position = positionRefernce.position + new Vector3( 0, offset, 0);
+            newLine.GetComponent<SpriteRenderer>().sprite = ingredient.item.sprite;
+            newLine.transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text = ingredient.ammount.ToString();
+            offset += offsetammount; 
+        }
+    }
+}
--- Assets/Scripts/Ingredients.cs
+++ Assets/Scripts/Ingredients.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 [System.Serializable]
 public class Item
 {
-    int id;
-    string name;
-    Sprite sprite; 
+    public int id;
+    public string name;
+    public Sprite sprite; 
 
     public Item(int id, string name, Sprite sprite)
     {
         this.id = id;
         this.name = name;
-        this.sprite = sprite;
+        this.sprite = Resources.Load<Sprite>("Sprites/" + name);
+    }
+
+    public Item(Item item)
+    {
+        this.id = item.id;
+        this.name = item.name;
+        this.sprite = Resources.Load<Sprite>("Sprites/" + item.name);
     }
 
 }
