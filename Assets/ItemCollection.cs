c6b438bdc2023a7c9e24e28282ed3e76fcf7d0fa
feldbacherSimone <simone.feldbacher03@htl-salzburg-ac.at>
2021-05-05 17:51:48 +0200

    Implement inventory bracelette

---
+++ Assets/ItemCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollection : MonoBehaviour
+{
+
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Item")
+        {
+            //Destroy(collision.gameObject);
+            collision.gameObject.SetActive(false);
+        }
+
+        print("collision");
+    }
+}
