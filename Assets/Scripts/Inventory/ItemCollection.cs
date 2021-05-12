fb728080a0c6721abc88e116fb6525808e2fc833
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-05-12 18:03:07 +0200

    add item recognition

---
+++ Assets/Scripts/Inventory/ItemCollection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollection : MonoBehaviour
+{
+
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Item")
+        {
+            //Destroy(collision.gameObject);
+
+            Recource recource = collision.gameObject.GetComponent<Recource>();
+
+            AddItemToInventory(recource);
+
+            collision.gameObject.SetActive(false);
+        }
+
+        print("collision");
+    }
+
+
+    void AddItemToInventory(Recource recource)
+    {
+
+        foreach (Item item in ItemDatabase.instance.items)
+        {
+            if (recource.ItemName == item.name)
+            {
+                item.amountCollectd++;
+                print(item.name + "was collected");
+                ItemDatabase.instance.showItemAmmount();
+            }
+      
+        }
+    }
+}
