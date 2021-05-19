e90a457f80509ab6a3e9a4b2a23895d2581bb5ff
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-05-19 17:46:49 +0200

    add Item Pickuop drops

--- Assets/Scripts/Inventory/Collectable.cs
+++ Assets/Scripts/Inventory/Collectable.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     public int objectHealth;
-    public int inventoryID; 
-    
+    public string itemName;
+    public GameObject shardTemplate;
+    public int itemCount = 3;
+
+    [SerializeField] public static float itemForce = 0.1f;
     public void destroy()
+        
     {
         //instantiate shards and add force to them so they fly away 
         //shards could be saved in a prefab 
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            GameObject shard = GameObject.Instantiate(shardTemplate);
+            shard.transform.position = gameObject.transform.position; 
+            shard.GetComponent<Recource>().ItemInit(itemName, itemForce);
+        }
 
         GameObject.Destroy(gameObject);
     }
--- Assets/Scripts/Inventory/Recource.cs
+++ Assets/Scripts/Inventory/Recource.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Recource : MonoBehaviour
 {
-    public string ItemName; 
+    public string ItemName;
+
+    public void ItemInit(string name, float forceAmmount )
+    {
+      
+        ItemName = name;
+        foreach (Item item in ItemDatabase.instance.items)
+        {
+            if (ItemName == item.name)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = item.sprite;
+                gameObject.GetComponent<Rigidbody>().AddRelativeForce(Random.onUnitSphere * forceAmmount);
+
+            }
+          
+        }
+    }
    
 }
