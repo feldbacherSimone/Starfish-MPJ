4a03e2346a14298a5818e1d2c61791dbe9e551fe
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-06-12 16:08:21 +0200

    implement settings menu

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio; 
 
 public class StartMenu : MonoBehaviour
 {
-
-
+    float currentVolume;
+    float minVol = -20;
+    float maxVol = 20;
+    public AudioMixerGroup mixerGroup;
     public void StartGame(int sceneIndex)
     {
        
@@ -21,5 +24,12 @@ public class StartMenu : MonoBehaviour
     {
         Application.Quit();
         print("QuitGame");
+    }
+
+    public void SoundVolume( int addAmmount)
+    {
+        mixerGroup.audioMixer.GetFloat("masterVol", out currentVolume);
+
+        mixerGroup.audioMixer.SetFloat("masterVol", currentVolume + addAmmount);
     }
 }
