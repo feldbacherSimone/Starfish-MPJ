be73c5c7811db8bcf38a1d1bbf0ffa80bb5c1aa3
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-06-13 20:25:10 +0200

    add way pointers and fix window mat (also the ship is rainbow inside now, pride month or smthing)

---
+++ Assets/LocationDesc.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LocationDesc : MonoBehaviour
+{
+    GameObject player;
+    public float distance;
+    public GameObject[] arrows;
+    string text; 
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        text = gameObject.GetComponent<TextMeshProUGUI>().text;
+   
+    }
+    private void Update()
+    {
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < distance)
+        {
+            gameObject.GetComponent<TextMeshProUGUI>().text = null;
+            StartCoroutine(ShowArrows());
+        }
+
+        else
+        {
+            gameObject.GetComponent<TextMeshProUGUI>().text = text;
+        }
+            print("distance = " + Vector3.Distance(player.transform.position, gameObject.transform.position));
+
+    }
+
+    IEnumerator ShowArrows()
+    {
+        int i = 0; 
+        while (i < arrows.Length)
+        {
+            arrows[i].SetActive(true);
+            i++;
+            yield return new WaitForSeconds(0.2f);
+        }
+        yield return new WaitForSeconds(0.5f);
+    }
+}
